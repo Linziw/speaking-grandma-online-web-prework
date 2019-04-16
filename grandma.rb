db7bdd779d8faa51cb61a87a50750887dd721dbd
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-16 13:06:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,7 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(input)
+  if input == "test"
+    puts string
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
