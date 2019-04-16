e595b6b09049f88932d0615c9b9dc3bbae95446f
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-16 13:11:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@ def speak_to_grandma(input)
   if input == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
     elsif input == input.upcase
+    puts 'NO, NOT SINCE 1938!'
+  else 
     puts 
 end
 
